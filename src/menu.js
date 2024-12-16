afb3c25e987fe4a0123b2753232519b3704648c5
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2024-12-15 22:57:21 -0800

    Add menu page

--- src/menu.js
+++ src/menu.js
@@ -1,2 +1,37 @@
+import img1 from "../image-1.jpg";
+import img2 from "../image-2.jpg";
+
 export const menuPage = `
+<div class="menu-container">
+            <div class="menu-card">
+                <img src="${img1}" alt="">
+                <h3>Suggary classic</h3>
+                <p>The classic</p>
+            </div>
+            <div class="menu-card">
+                <img src="${img2}" alt="">
+                <h3>Sprinkle delight</h3>
+                <p>Fun sprinkles</p>
+            </div>
+            <div class="menu-card">
+                <img src="${img1}" alt="">
+                <h3>Chocolate Fun</h3>
+                <p>Covered in chocolaet</p>
+            </div>
+            <div class="menu-card">
+                <img src="${img2}" alt="">
+                <h3>Chocolate Fun</h3>
+                <p>Covered in chocolaet</p>
+            </div>
+            <div class="menu-card">
+                <img src="${img1}" alt="">
+                <h3>Chocolate Fun</h3>
+                <p>Covered in chocolaet</p>
+            </div>
+            <div class="menu-card">
+                <img src="${img2}" alt="">
+                <h3>Chocolate Fun</h3>
+                <p>Covered in chocolate</p>
+            </div>
+        </div>
 `
